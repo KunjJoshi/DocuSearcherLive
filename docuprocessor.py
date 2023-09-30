0065085a6bb9265cf81cc8717f9fe54bca8e93a7
Kunj Joshi <93201673+KunjJoshi@users.noreply.github.com>
2023-10-01 01:00:04 +0530

    init

---
+++ docuprocessor.py
@@ -0,0 +1,42 @@
+from videoprocessor import audio_process, video_process
+from imageprocessor import image_processing
+from textprocessor import processText
+from pdfprocessor import processpdf
+import os
+
+def processdocs(dataset, docpath):
+    try:
+        docist=docpath.split('.')
+        format=docist[1]
+        if format == 'wav':
+            #its an audio file
+            audiotext=audio_process(dataset, docpath)
+            print(audiotext)
+        elif format=='mp4':
+            #its a video file
+            videotext=video_process(dataset, docpath)
+            print(videotext)
+        elif format=='png' or format=='jpg' or format=='jpeg':
+            #its an image
+            imgtext=image_processing(dataset, docpath)
+            print(imgtext)
+        elif format=='pdf':
+            #its a pdf file
+            pdftext=processpdf(dataset, docpath)
+            print(pdftext)
+        elif format=='txt' or format=='docx' or format=='doc':
+            #its a text file
+            text=processText(dataset, docpath)
+            print(text)
+    except Exception as e:
+        print(e)
+
+
+example_doc_list=[f"Workforce/{file}" for file in os.listdir('Workforce')]
+
+for file in example_doc_list:
+    print(f'Starting to process {file}')
+    processdocs('Dataset.csv',file)
+    print(f'Finished Processing {file}')
+
+
---
+++ search.py
@@ -0,0 +1,36 @@
+from googlelinkbringer import getGooglePages
+import pandas as pd
+def makeSearch(query, opfilename):
+    df=pd.read_csv('Dataset.csv')
+    alltexts=list(df['Text'])
+    allpaths=list(df['Paths'])
+    results=[]
+    try:
+        for ind,text in enumerate(alltexts):
+            resdict={}
+            if query in text:
+                resdict['Text']=text
+                resdict['Query']=query
+                resdict['Position']=text.find(query)
+                resdict['Path']=allpaths[ind]
+                results.append(resdict)
+        data=f'\t\tFound Results for Query: {query}'
+        for result in results:
+            data=data+'Found Query in File: '+result['Path']+'\t Found Text in File: '+result['Text']+'\tFound Query at Position: '+result['Position']+'\t\n'
+        googleLinks=getGooglePages(query)
+        data=data+'\n\t\tFound Some More results for you from Google. FOllow the below given links: \n'
+        for link in googleLinks:
+            data=data+' '+link+'\n'
+        with open(opfilename,'w') as file:
+            file.write(data)
+        return 'Search Results Found'
+    except Exception as e:
+        print(e)
+        return 'Search Failed'
+
+searchSuccess=makeSearch('government','governmentResults.txt')
+print('Government Result: ', searchSuccess)
+
+searchSuccess=makeSearch('food','foodResults.txt')
+print('Food Result: ', searchSuccess)
+
